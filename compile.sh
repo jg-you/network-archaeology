3bd42c313ec47c8090dcce09fb352364dd0e9fb1
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2019-05-12 16:07:51 -0400

    Remove old C++ Importance sampling method

--- compile.sh
+++ compile.sh
@@ -4,15 +4,8 @@ echo "[Entering $PWD]"
 echo "[Compiling OD.cpp]"
 g++ -o3 OD.cpp -o OD
 echo "[Compilation of OD.cpp done]"
-# Compile sampler
-cd importance_sampling_cpp/
-echo "[Entering $PWD]"
-echo "[Launching compilation of the sampler (CMAKE)]"
-cmake .;
-make
-echo "[Compilation of the sampler done]"
 # Go back to root
-cd ../../../
+cd ../../
 echo "[Entering $PWD]"
 # Compile generator
 cd generators
